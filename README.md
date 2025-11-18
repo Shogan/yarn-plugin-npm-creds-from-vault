9b5ba4083eb060625365d67598f21df20f5bd463
Sean <shogan85@gmail.com>
2025-11-18 22:09:18 +0000

    feat: add encrypt token command, change config, update docs

--- README.md
+++ README.md
@@ -4,27 +4,49 @@ Avoid storing npm authentication tokens in plaintext on your filesystem.
 
 A plugin to hook the npm authentication process for a selected registry, and retrieve the npm auth token from your own local encrypted storage vault.
 
-Currently only supports GPG-encrypted vault files (.gpg).
+## Requirements
 
-## Installation
+Currently only supports GPG (OpenPGP) encrypted vault files (.gpg). This tooling is therefore a requirement for this plugin to work. You can install gnupg on macOS for example using homebrew. Alternatively, for other platforms, the standard methods can be followed to install there.
 
-To install the plugin, run the following command in your terminal:
+macOS example:
 
 ```bash
-yarn plugin import https://github.com/Shogan/yarn-plugin-npm-creds-from-vault/releases/download/v0.0.1/plugin-npm-creds-from-vault.js
+brew install gnupg
+# optionally if you want to configure gpg to use pinentry-mac for passphrase prompts (e.g. in combination with a hardware token like a Yubikey, you may want to install pinentry-mac as well)
+brew install pinentry-mac
+```
+
+## Plugin installation
+
+To install the plugin, run the following command in your terminal (where you specify the release version to install):
+
+```bash
+yarn plugin import https://github.com/Shogan/yarn-plugin-npm-creds-from-vault/releases/download/v0.0.3/plugin-npm-creds-from-vault.js
 ```
 
 ## Configuration
-To configure the plugin, add the following section to your `.yarnrc.yml` file:
+To configure the plugin, add the following section to your `.yarnrc.yml` file (at either the project or global level):
 
 ```yaml
-encryptedTokenAbsoluteFilePath: /Users/exampleUserName/your_encrypted_npm_file.gpg
-registry: "https://npm.pkg.github.com" # The registry you want to use the encrypted token for
+# see below for how to create this encrypted file yourself.
+encryptedTokenAbsoluteFilePath: /Users/exampleUserName/my_encrypted_npm_token.gpg
+# list of registries you want to use the encrypted token value for. The hook will only be applied for these registries.
+# others not included in this list will use normal yarn/npm auth mechanisms, sourcing the token from .yarnrc.yml etc...
+includedRegistries:
+  - https://npm.pkg.github.com/my-org
 ```
 
-If the registry you select does not match the registry your package is being installed from, then the plugin will not attempt to retrieve the token from the vault, and will fall back to the default Yarn/npm authentication process.
+## Encrypting your npm token file
+
+You can use GPG yourself to encrypt a plaintext file containing your npm auth token, or use the `encrypt token` command provided by the plugin as a convenience wrapper around GPG. You will need to know your GPG key ID (recipient user ID) to use this command. You can list your keys with the `gpg --list-keys` command.
+
+```bash
+yarn encrypt token --token YOUR_NPM_TOKEN_GOES_HERE --gpg-key-id 1234CAB48AC641ECFAA1B82CDED29FA111ABCDEF1 --output ~/my_encrypted_npm_token.gpg
+```
 
 ## Usage
+
+Usage is automatic. Once the plugin is installed and configured, it will automatically hook into yarn's npm authentication process, and invoke gpg to decrypt your token from file when needed. When you attempt to install a package from a registry, the plugin will decrypt the vault file, extract the npm auth token, and provide it to yarn for authentication.
 
 1. The plugin hooks the `getNpmAuthenticationHeader` method that yarn uses to retrieve the npm auth token for a given registry.
 2. When you first try to install a new package(s) from the selected registry, the plugin will attempt to decrypt the vault file using GPG. GPG's `--decrypt` command is invoked as a child process, so make sure you have GPG installed and properly configured on your system. If required, GPG will prompt you for your passphrase or pin etc...
--- sources/index.ts
+++ sources/index.ts
@@ -1,3 +1,4 @@
+import { BaseCommand } from '@yarnpkg/cli';
 import {
   Plugin,
   SettingsType,
@@ -5,6 +6,7 @@ import {
   Configuration,
 } from '@yarnpkg/core';
 import { exec } from 'child_process';
+import { Option } from 'clipanion';
 
 function run(cmd: string) {
 	return new Promise((resolve, reject) => {
@@ -19,7 +21,40 @@ function run(cmd: string) {
 declare module '@yarnpkg/core' {
   interface ConfigurationValueMap {
     encryptedTokenAbsoluteFilePath: string;
-    registry: string;
+    includedRegistries: string[];
+  }
+}
+
+class EncryptTokenCommand extends BaseCommand {
+  static paths = [
+    [`encrypt`, `token`],
+  ];
+
+  token = Option.String(`--token`, ``, {
+    description: `The encrypted value/string to store with GPG`,
+  });
+
+  recipientUserId = Option.String(`--gpg-key-id`, ``, {
+    description: `The 'recipient USER ID' (GPG key id) to use to encrypt the token`,
+  });
+
+  outputFilePath = Option.String(`--output`, ``, {
+    description: `The absolute file path to store the encrypted token .gpg file`,
+  });
+
+  async execute() {
+    if (!this.token) {
+      throw new Error('Token is required. Please provide a value using the --token option.');
+    }
+    if (!this.recipientUserId) {
+      throw new Error('GPG key id is required. Please provide a value using the --gpg-key-id option.');
+    }
+    if (!this.outputFilePath) {
+      throw new Error('Output file path is required. Please provide a value using the --output option.');
+    }
+
+    await run(`echo ${this.token} | gpg --encrypt -r ${this.recipientUserId} -o ${this.outputFilePath}`);
+    console.log(`Token stored at ${this.outputFilePath}`);
   }
 }
 
@@ -30,8 +65,9 @@ const plugin: Plugin = {
       type: SettingsType.STRING,
       default: null,
     },
-    registry: {
-      description: 'The registry that the decrypt hook should target',
+    includedRegistries: {
+      description: 'Optional list of registries that the decrypt hook should NOT target. If not provided, the plugin will attempt to use the encrypted token for all registries.',
+      isArray: true,
       type: SettingsType.STRING,
       default: null,
     },
@@ -43,36 +79,36 @@ const plugin: Plugin = {
 				configuration: Configuration;
 				ident?: Ident;
     }) => {
-      const configuredRegistry = configuration.get('registry');
+      const includedRegistries = configuration.get('includedRegistries');
       const configuredGpgTokenAbsPath = configuration.get('encryptedTokenAbsoluteFilePath');
-      if (configuredRegistry !== null && configuredRegistry !== undefined) {
-        if (registry !== configuredRegistry) {
-          console.log(`Skipping decryption for registry: ${registry} (we're only hooked for: ${configuredRegistry})`);
-          return currentHeader;
+      if (includedRegistries !== null && includedRegistries !== undefined) {
+        for (const includedRegistry of includedRegistries as string[]) {
+          if (registry === includedRegistry) {
+            console.log(`Using gpg encrypted token for registry: ${registry}`);
+            let tokenFilePath = configuredGpgTokenAbsPath || process.env.GPG_SOURCE_TOKEN_FILE_PATH;
+            if (!tokenFilePath) {
+              throw new Error('No GPG token absolute file path or GPG source token file path (env variable GPG_SOURCE_TOKEN_FILE_PATH) configured');
+            }
+
+            return run(`gpg --decrypt ${tokenFilePath}`)
+              .then((decryptedString) => {
+                const token = String(decryptedString).trim();
+                console.log(`Decrypted token for registry: ${registry}`);
+                return token;
+              })
+              .catch((error) => {
+                console.error(`Failed to decrypt token stored at ${tokenFilePath} for registry ${registry}:`, error);
+                return currentHeader;
+              });
+          }
         }
       }
-      
-      console.log(`Getting npm authentication token for registry: ${registry}`);
-      console.log(`Current header: ${currentHeader}`);
-      
-      let tokenFilePath = configuredGpgTokenAbsPath || process.env.GPG_SOURCE_TOKEN_FILE_PATH;
-      if (!tokenFilePath) {
-        throw new Error('No GPG token absolute file path or GPG source token file path (env variable GPG_SOURCE_TOKEN_FILE_PATH) configured');
-      }
 
-      return run(`gpg --decrypt ${tokenFilePath}`)
-        .then((decryptedString) => {
-          const token = String(decryptedString).trim();
-          console.log(`Decrypted token for registry: ${registry}`);
-          return token;
-        })
-        .catch((error) => {
-          console.error(`Failed to decrypt token stored at ${tokenFilePath} for registry ${registry}:`, error);
-          return currentHeader;
-        });
+      console.log(`Skipping decryption for registry: ${registry}`);
+      return currentHeader;
     },
   },
-  commands: [],
+  commands: [EncryptTokenCommand],
 };
 
 export default plugin;
